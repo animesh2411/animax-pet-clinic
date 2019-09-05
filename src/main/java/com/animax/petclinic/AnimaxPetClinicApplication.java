14253557a5ff4a5d4e6705be9a8b269baa74af31
Animesh Gupta <animesh2411@gmail.com>
2019-09-06 01:10:42 +0530

    First Commit added installation files

---
+++ src/main/java/com/animax/petclinic/AnimaxPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.animax.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaxPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaxPetClinicApplication.class, args);
+	}
+
+}
